9033e611f5346827355874faba148d071f4c7d18
Chris Williams <chrispwill@gmail.com>
2015-08-27 23:20:14 +1000

    Minor changes.

--- src/cpu.rs
+++ src/cpu.rs
@@ -3,23 +3,23 @@ use mem::MappedMem;
 /// The CPU status flags (known as the P register)
 struct Status
 {
-    c: bool,
-    z: bool,
-    i: bool,
-    d: bool,
-    b: bool,
-    v: bool,
-    n: bool,
+    c: bool, // (C)  Carry Flag
+    z: bool, // (Z)  Zero Flag
+    i: bool, // (I)  Interrupt Disable
+    d: bool, // (D)  Decimal Mode
+    b: bool, // (B)  Break Command
+    v: bool, // (V)  Overflow Flag
+    n: bool, // (N)  Negative Flag
 }
 
 /// The CPU's registers
 struct Regs
 {
-    pc: u16,
-    sp: u8,
-    a: u8,
-    x: u8,
-    y: u8,
+    pc: u16, // (PC) Program Counter
+    sp: u8,  // (SP) Stack Pointer
+    a: u8,   // (A)  Accucmulator
+    x: u8,   // (X)  Index Register X
+    y: u8,   // (Y)  Index Register Y
     status: Status,
 }
 
