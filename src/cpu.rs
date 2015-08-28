48c2649902dc738cfff7935966517e5293916382
Chris Williams <chrispwill@gmail.com>
2015-08-28 13:04:26 +1000

    Initialising registers

--- src/cpu.rs
+++ src/cpu.rs
@@ -12,6 +12,13 @@ struct Status
     n: bool, // (N)  Negative Flag
 }
 
+impl Status {
+    fn new() -> Status
+    {
+        Status { c: true, z: true, i: true, d: true, b: true, v: true, n: true }
+    }
+}
+
 /// The CPU's registers
 struct Regs
 {
@@ -21,6 +28,13 @@ struct Regs
     x: u8,   // (X)  Index Register X
     y: u8,   // (Y)  Index Register Y
     status: Status,
+}
+
+impl Regs {
+    fn new() -> Regs
+    {
+        Regs{ pc: 0x8000, sp: 0xFF, a: 0x00, x: 0x00, y: 0x0, status: Status::new() }
+    }
 }
 
 /// The CPU structure
